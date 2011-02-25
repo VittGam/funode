b8d7ea145d0f83e2c6eec29101377faff359f394
fogus <mefogus@gmail.com>
2011-02-25 11:36:50 -0500

    Added impl of Tramp.oline and Seq.rest

--- src/seq/core.js
+++ src/seq/core.js
@@ -1,7 +1,11 @@
 var Seq = new Object;
 
-Seq.first = function(array) {
-  return array[0];
+Seq.first = function(array_like) {
+  return array_like[0];
+};
+
+Seq.rest = function(array_like) {
+  return Array.prototype.slice.call(array_like, 1);
 };
 
 
--- src/tramp/core.js
+++ src/tramp/core.js
@@ -1,7 +1,20 @@
 var Tramp = new Object;
 
-Tramp.oline = function(firstFun) {
-  return firstFun();
+Tramp.oline = function(fun) {
+  var ret = null;
+
+  if (arguments.length > 1) {
+    ret = fun.apply(fun, Array.prototype.slice.call(arguments, 1));
+  }
+  else {
+    ret = fun();
+  }
+
+  while (typeof ret == 'function') {
+    ret = ret();
+  }
+
+  return ret;
 };
 
 
